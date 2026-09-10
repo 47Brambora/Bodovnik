39f50bf1000795605974c8f780619900ac28dbc3
47Brambora <47Brambora@gmail.com>
2026-09-10 20:29:48 +0000

    feat: add member edit layout and view swiching logic

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,16 @@
 // Render tabulek, vstupních polí a přepínání záložek
+
+const btnEditMembers = document.getElementById('welc-msg-but-edit-mem');
+const welcomeMsg = document.getElementById('welcome-msg');
+const editMembers = document.getElementById('edit-members');
+const btnToWelcomePage = document.getElementById('to-welcome-page');
+
+btnEditMembers.addEventListener('click', () => {
+    welcomeMsg.classList.add('hidden');
+    editMembers.classList.remove('hidden');
+});
+
+btnToWelcomePage.addEventListener('click', () => {
+    welcomeMsg.classList.remove('hidden');
+    editMembers.classList.add('hidden');
+})
