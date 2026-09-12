c9b9ae5b272a1a02a63e32fdf24ae508d31361aa
47Brambora <47Brambora@gmail.com>
2026-09-12 19:29:12 +0000

    feat: implement member addition form and UI visibility controls

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,27 @@
 // Render tabulek, vstupních polí a přepínání záložek
 
-const btnEditMembers = document.getElementById('welc-msg-but-edit-mem');
+const btnEditMembers = document.getElementById('btn-edit-mem');
 const welcomeMsg = document.getElementById('welcome-msg');
 const editMembers = document.getElementById('edit-members');
 const btnToWelcomePage = document.getElementById('to-welcome-page');
+const addMember = document.getElementById('add-member');
+const editMembersAddMember = document.getElementById('btn-add-member');
+const hideEditMembersAddMember = document.getElementById('btn-hide-add-member');
+
 
 btnEditMembers.addEventListener('click', () => {
     welcomeMsg.classList.add('hidden');
     editMembers.classList.remove('hidden');
 });
 
+editMembersAddMember.addEventListener('click', () => {
+    addMember.classList.remove('hidden');
+});
+hideEditMembersAddMember.addEventListener('click', () => {
+    addMember.classList.add('hidden');
+});
+
 btnToWelcomePage.addEventListener('click', () => {
     welcomeMsg.classList.remove('hidden');
     editMembers.classList.add('hidden');
-})
+});
